786880888a943e2f50f495f999dd56a1bf516c08
Goutami1721 <gautbhosale@gmail.com>
2023-01-25 22:57:14 -0800

    for gitpractice

---
+++ src/demopackages/forgitpractice1.java
@@ -0,0 +1,24 @@
+package demopackages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class forgitpractice1 {
+
+	@Test
+	public void geturl()
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void print()
+	{
+		System.out.println("hello");
+	}
+
+}
